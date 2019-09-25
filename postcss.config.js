aee943f0ca23a0fb08d5fb1d6b5d468349039312
Castro <ceafive@gmail.com>
2019-09-25 13:52:21 +0000

    first upload

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+const tailwindcss = require('tailwindcss')
+const purgecss = require("@fullhuman/postcss-purgecss")
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+    plugins: [
+        tailwindcss('./tailwind.config.js'),
+        cssnano({
+            preset: 'default'
+        }),
+        purgecss({
+            content:['*.html'],
+            defaultExtractor: content => content.match(/[A-Za-z)-9-_:/]+/g) || []
+        }),
+        autoprefixer
+    ]
+}
+
+
+
+
+
+
+   
