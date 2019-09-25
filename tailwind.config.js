aea16a5bb09468a80d675fc79150e1db2b10ef0e
Castro <ceafive@gmail.com>
2019-09-25 13:37:07 +0000

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+        'body': 'Lato',
+    },
+    extend: {
+        colors: {
+            "devblue": "#184cd5",
+            "main_blue": "#0a0e13",
+            "main_gold": "#ab9a75",
+            "main_green": "#00ea86",
+            "main_green2": "#00c873"
+        },
+        fontSize: {
+            '8xl': '5rem',
+            '10xl': '8rem',
+            '12xl': '10rem',
+            '20xl': '20rem'
+        },
+        padding: {
+            '72': '24rem',
+            '105': '34rem'
+        }
+    },
+    screens: {
+      'xs': '375px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xl2': '1630px'
+    }
+  },
+  variants: {
+        borderRadius: ['responsive', 'hover', 'focus'],
+        padding: ['responsive', 'hover', 'focus'],
+  },
+  plugins: []
+}
